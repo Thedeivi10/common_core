83196f91f12cf379e262379f2324140710957abd
Thedeivi10 <thedeivi@DESKTOP-TJ1QATB>
2024-02-29 12:39:42 +0100

    clear

---
+++ libft/libft/libft.h
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libft.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: davigome <davigome@studen.42malaga.com>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/02/24 22:08:21 by davigome          #+#    #+#             */
+/*   Updated: 2024/02/25 17:19:04 by davigome         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LIBFT_H
+# define LIBFT_H
+
+int	ft_isalpha(int c);
+int	ft_isdigit(int c);
+int	ft_isalnum(int c);
+int	ft_isascii(int c);
+
+#endif
---
+++ libft/libft/prueba.c
@@ -0,0 +1,7 @@
+#include "libft.h"
+#include "stdio.h"
+int	main(void)
+{
+	printf("%s", "Si es un numero saldra 1: ");
+	printf("%d",ft_isdigit('3'));
+}
