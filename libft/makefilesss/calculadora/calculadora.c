2576d8ae4dca9ec08f15978c7324bb85a714207c
David Gomez-Landero Lopez <davigome@c3r4s5.42malaga.com>
2024-04-19 09:42:32 +0200

    cambio de carpeta y limpieza

--- libft/makefilesss/calculadora/calculadora.c
+++
@@ -1,6 +0,0 @@
-#include "funciones.h"
-
-int	sumar(int a, int b)
-{
-	return a +  b;
-}
--- libft/makefilesss/calculadora/salida.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-#include "funciones.h"
-
-void	presentar(int a)
-{
-	printf("La respuesta es %d.\n", a);
-}
