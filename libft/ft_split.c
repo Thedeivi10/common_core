cde72f85817798a846aabe48a5f8ede618f791bd
Thedeivi10 <d.gomezlanderolopez@gmail.com>
2024-04-29 07:51:34 +0200

    split de momento

--- libft/ft_split.c
+++ libft/ft_split.c
@@ -6,7 +6,7 @@
 /*   By: davigome <davigome@studen.42malaga.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/28 08:51:11 by davigome          #+#    #+#             */
-/*   Updated: 2024/04/28 12:09:20 by davigome         ###   ########.fr       */
+/*   Updated: 2024/04/29 07:51:09 by davigome         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,11 +16,10 @@ char	**ft_split(char conts *s, char c)
 {
 	int	*characters;
 	int	row;
-	int	col;
+	int	j;
 	int i;
-
+	char **arrays;
 	row = 0;
-	col = 0;
 	i = -1;
 	if (!s || !c)
 		return NULL
@@ -34,5 +33,35 @@ char	**ft_split(char conts *s, char c)
 			characters[row] = 0;
 		}
 	}
-
+	arrays= (char **)malloc(row * sizeof(char *));
+	if (!arrays)
+		return (0);
+	i = -1;
+	while (++i < row)
+	{
+		arrays[i] = (char *)malloc(characters[i] * sizeof(char));
+		if (!arrays[i])
+		{
+			while (0 < i)
+			{
+				free(arrays[i])
+				i--;
+				return (0);
+			}
+		}
+	}
+	i = -1;
+	int k;
+	k = -1;
+	j = 0;
+	while (s[++i])
+	{
+		k = -1;
+		while (characters[j] > ++k)
+			arrays[j][k] = s[i];
+		arrays[j][k] = 0;
+		j++;
+	}
+	arrays[j][0] = 0;
+	return (arrays);
 }
