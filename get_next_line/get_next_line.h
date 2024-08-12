3c295d1bc9bcbd20ac5ffb48c37f3b0c48609a2f
Thedeivi10 <d.gomezlanderolopez@gmail.com>
2024-08-12 11:04:14 +0200

    get_next_line library

--- get_next_line/get_next_line.h
+++ get_next_line/get_next_line.h
@@ -6,7 +6,16 @@
 /*   By: davigome <davigome@studen.42malaga.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/11 01:13:28 by davigome          #+#    #+#             */
-/*   Updated: 2024/08/11 01:13:29 by davigome         ###   ########.fr       */
+/*   Updated: 2024/08/11 17:49:10 by davigome         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef GET_NEXT_LINE_H
+#define GET_NEXT_LINE_H
+
+#include <fcntl.h>
+# include <stdlib.h>
+# include <unistd.h>
+
+
+#endif
