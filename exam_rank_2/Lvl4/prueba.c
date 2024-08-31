5b9916833971bf5f01da128a1c3b4026d7157baf
Thedeivi10 <d.gomezlanderolopez@gmail.com>
2024-08-31 08:13:36 +0200

    vamooooo

---
+++ exam_rank_2/Lvl4/prueba.c
@@ -0,0 +1,11 @@
+typedef struct      s_list
+{
+    struct s_list   *next;
+    void            *data;
+}                   t_list;
+
+void ft_list_remove_if(t_list **begin_list, void *data_ref, int (*cmp)())
+{
+	t_list *aux = *begin_list;
+	
+}
