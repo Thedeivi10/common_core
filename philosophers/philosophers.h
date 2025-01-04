935e5dd67a5934ae22ccc42b6e5a3c1ce425a7d1
Thedeivi10 <d.gomezlanderolopez@gmail.com>
2025-01-05 00:32:10 +0100

    push swap makefile corrected, every thing ok

---
+++ philosophers/philosophers.h
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philosophers.h                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: davigome <davigome@studen.42malaga.com>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/01/04 19:57:21 by davigome          #+#    #+#             */
+/*   Updated: 2025/01/05 00:27:48 by davigome         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHILOSOPHERS_H
+# define PHILOSOPHERS_H
+
+# include <unistd.h>
+# include <stdlib.h>
+# include <sys/time.h>
+# include <pthread.h>
+
+//	FT_CHECK.C //
+void	ft_check(char **argv);
+
+#endif
