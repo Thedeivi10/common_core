2c0f2a1c103fccecc5daf208b14b9f897b6736a9
Thedeivi10 <d.gomezlanderolopez@gmail.com>
2025-05-21 08:03:35 +0200

    deleted netpractice.1.5 and almost done cpp01

--- cpp/01/ex03/HumanA.hpp
+++ cpp/01/ex03/HumanA.hpp
@@ -6,7 +6,22 @@
 /*   By: davigome <davigome@studen.42malaga.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/19 19:22:48 by davigome          #+#    #+#             */
-/*   Updated: 2025/05/19 19:22:49 by davigome         ###   ########.fr       */
+/*   Updated: 2025/05/21 08:01:56 by davigome         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <iostream>
+#include <string>
+#include "Weapon.hpp"
+
+class HumanA{
+
+	private:
+
+		std::string name;
+		Weapon weapon;
+	public:
+	HumanA(std::string weapon);
+	void attack();
+	~HumanA();
+};
