0ba0d1b39553b17faa7992c2b78a9e2f08337bb9
David Gomez-Landero Lopez <davigome@c3r4s5.42malaga.com>
2025-06-16 21:47:16 +0200

    cpp06

--- cpp/06/ex00/ScalarConverter.cpp
+++ cpp/06/ex00/ScalarConverter.cpp
@@ -6,7 +6,7 @@
 /*   By: davigome <davigome@studen.42malaga.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/15 22:18:35 by davigome          #+#    #+#             */
-/*   Updated: 2025/06/16 11:56:36 by davigome         ###   ########.fr       */
+/*   Updated: 2025/06/16 18:26:59 by davigome         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,20 +26,171 @@ ScalarConverter &ScalarConverter::operator=(const ScalarConverter &other){
 	return *this;
 }
 
-std::string*	isChar(const std::string &argv, std::string *type)
+void	ScalarConverter::isChar(const std::string &argv, std::string &type)
 {
-	if (argv.length() > 1)
-	{
-		type = NULL;
-		return type;
-	}
+	if (argv.length() > 1 || !std::isprint(argv[0]) || std::isdigit(argv[0]))
+		return ;
+	type = "CHAR";
+}
+
+void	ScalarConverter::isInt(const std::string &argv, std::string &type)
+{
+	errno = 0;
+	char *endptr = NULL;
+
+	long val = std::strtol(argv.c_str(), &endptr, 10);
+	if (errno == ERANGE || *endptr != '\0' || val < INT_MIN || val > INT_MAX)
+		return ;
+	type = "INT";
+}
+
+void	ScalarConverter::isFloat(const std::string &argv, std::string &type)
+{
+	std::string num;
 	
+	if (argv == "-inff" || argv == "+inff" || argv == "nanf")
+	{
+		type = "FLOAT";
+		return ;
+	}
+	if (argv[argv.length() - 1] != 'f' || argv.length() < 3)
+		return ;
+	num = argv.substr(0, argv.length() - 1);
+	char *endptr = NULL;
+	errno = 0;
+	float val = std::strtof(num.c_str(), &endptr);
+	if (*endptr != '\0' || errno == ERANGE)
+		return ;
+	int i = -1;
+	int count = 0;
+	while (argv[++i])
+	{
+		if (argv[i] == '.')
+			++count;
+	}
+	if (count != 1)
+		return ;
+	i = num.find('.');
+	num = num.substr(i + 1);
+	if (num == "" || num.length() != 1 || !std::isdigit(num[0]))
+		return ;
+	(void)val;
+	type = "FLOAT";
+}
+
+void	ScalarConverter::isDouble(const std::string &argv, std::string &type)
+{
+	std::string num;
+	
+	if (argv == "-inf" || argv == "+inf" || argv == "nan")
+	{
+		type = "DOUBLE";
+		return ;
+	}
+	if (argv.length() < 2)
+		return ;
+	num = argv;
+	char *endptr = NULL;
+	errno = 0;
+	double val = std::strtod(num.c_str(), &endptr);
+	if (*endptr != '\0' || errno == ERANGE)
+		return ;
+	int i = -1;
+	int count = 0;
+	while (argv[++i])
+	{
+		if (argv[i] == '.')
+			++count;
+	}
+	if (count != 1)
+		return ;
+	i = num.find('.');
+	num = num.substr(i + 1);
+	if (num == "" || num.length() != 1 || !std::isdigit(num[0]))
+		return ;
+	(void)val;
+	type = "DOUBLE";
+}
+
+void	ScalarConverter::printChar(const std::string &argv)
+{
+	std::cout << "char: " << argv[0] << std::endl;
+	int i = argv[0];
+	std::cout << "int: " << i << std::endl;
+	std::cout << "float: " << i << ".0f" << std::endl;
+	std::cout << "double: " << i << ".0" << std::endl;
+}
+
+void	ScalarConverter::printInt(const std::string &argv)
+{	
+	char *endptr = NULL;
+
+	long val = std::strtol(argv.c_str(), &endptr, 10);
+	std::cout << "char: ";
+	char c = val;
+	if (!isprint(c))
+		std::cout << "Non displayable" << std::endl;
+	else
+		std::cout << c << std::endl;
+	std::cout << "int: " << val << std::endl;
+	std::cout << "float: " << val<< ".0f" << std::endl;
+	std::cout << "double: " << val << ".0" << std::endl;
+}
+
+void	ScalarConverter::printFloat(const std::string &argv)
+{
+	std::string num;
+	
+	if (argv == "-inff" || argv == "+inff" || argv == "nanf")
+	{
+		std::cout << "char: impossible" << std::endl;
+		std::cout << "int : impossible" << std::endl;
+		std::cout << ""
+		 return ;
+	}
+	if (argv[argv.length() - 1] != 'f' || argv.length() < 3)
+		return ;
+	num = argv.substr(0, argv.length() - 1);
+	char *endptr = NULL;
+	errno = 0;
+	float val = std::strtof(num.c_str(), &endptr);
+	std::cout << "char: " << argv[0] << std::endl;
+	int i = argv[0];
+	std::cout << "int: " << i << std::endl;
+	std::cout << "float: " << i << ".0f" << std::endl;
+	std::cout << "double: " << i << ".0" << std::endl;
+}
+
+void	ScalarConverter::printDouble(const std::string &argv)
+{
+	std::cout << "char: " << argv[0] << std::endl;
+	int i = argv[0];
+	std::cout << "int: " << i << std::endl;
+	std::cout << "float: " << i << ".0f" << std::endl;
+	std::cout << "double: " << i << ".0" << std::endl;
 }
 
 void	ScalarConverter::convert(const std::string &argv)
 {
-	std::string *type;
-	type = NULL;
+	std::string type = "";
+	ScalarConverter converter;
 	
-	type = isChar(argv, type);
+	converter.isChar(argv, type);
+	converter.isInt(argv, type);
+	converter.isFloat(argv, type);
+	converter.isDouble(argv, type);
+	if (type == "")
+	{
+		std::cerr << "Invalid input" << std::endl;
+		return ;
+	}
+	if (type == "CHAR")
+		converter.printChar(argv);
+	if (type == "INT")
+		converter.printInt(argv);
+	if (type == "FLOAT")
+		converter.printFloat(argv);
+	if (type == "DOUBLE")
+		converter.printDouble(argv);
+	std::cout << type << std::endl;
 }
--- cpp/06/ex00/ScalarConverter.hpp
+++ cpp/06/ex00/ScalarConverter.hpp
@@ -6,13 +6,16 @@
 /*   By: davigome <davigome@studen.42malaga.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/15 22:18:50 by davigome          #+#    #+#             */
-/*   Updated: 2025/06/16 11:23:20 by davigome         ###   ########.fr       */
+/*   Updated: 2025/06/16 18:00:25 by davigome         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #pragma once
 
 #include <iostream>
+#include <cstdlib>
+#include <cerrno>
+#include <climits>
 
 class ScalarConverter
 {
@@ -23,5 +26,14 @@ class ScalarConverter
 		~ScalarConverter();
 		ScalarConverter(const ScalarConverter &other);
 		ScalarConverter &operator=(const ScalarConverter &other);
-		
+
+		void	isChar(const std::string &argv, std::string &type);
+		void	isInt(const std::string &argv, std::string &type);
+		void	isFloat(const std::string &argv, std::string &type);
+		void	isDouble(const std::string &argv, std::string &type);
+
+		void	printChar(const std::string &argv);
+		void	printInt(const std::string &argv);
+		void	printFloat(const std::string &argv);
+		void	printDouble(const std::string &argv);
 };
