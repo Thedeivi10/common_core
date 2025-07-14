83abf644bf64eb426de647a1bcf5ed98d7cdcc06
Thedeivi10 <d.gomezlanderolopez@gmail.com>
2025-07-14 22:39:03 +0200

    holamundo cursus done

---
+++ inception/pruebas/hola-mundo/app.js
@@ -0,0 +1,9 @@
+console.log("Hola mundo")
+ /**
+  * Lo necesario para poder levantar esta mini app es
+  * Sistema operativo
+  * Node JS
+  * Copiar
+  * node app.js
+  * TODO LO DEFINIMOS EN NUESTRO DOCKERFILE
+  */
